718740c02d3d7f321aab45ba37683d94b0df0989
alan <alan_henschel@hotmail.com>
2021-11-04 01:26:10 -0300

    fix: configuring prettier and eslint

--- src/components/inputTodo/inputTodo.jsx
+++ src/components/inputTodo/inputTodo.jsx
@@ -6,16 +6,16 @@ const inputTodo = () => {
   const [input, setInput] = useState('')
   const [todoList, setTodoList] = useState([])
   return (
-    <div className="container">
-      <div className="row justify-content-md-center">
-        <div className="col-sm-8 w-50">
-          <div className="input-group input-group-lg justify-content-center">
+    <div className='container'>
+      <div className='row justify-content-md-center'>
+        <div className='col-sm-8 w-50'>
+          <div className='input-group input-group-lg justify-content-center'>
             <input
-              type="text"
-              className="form-control p-3"
-              aria-label="Large"
-              aria-describedby="inputGroup-sizing-sm"
-              placeholder="O que você precisa fazer ?"
+              type='text'
+              className='form-control p-3'
+              aria-label='Large'
+              aria-describedby='inputGroup-sizing-sm'
+              placeholder='O que você precisa fazer ?'
               value={input}
               onChange={(event) => {
                 setInput(event.target.value)
@@ -31,10 +31,10 @@ const inputTodo = () => {
           <ul>
             <ItemTodo todoList={todoList} onDelete={setTodoList} />
           </ul>
-          <div className="card">
-            <div className="d-flex footer-padding">
+          <div className='card'>
+            <div className='d-flex footer-padding'>
               <span> 1 Itens faltando</span>
-              <div className="d-flex margin-left">
+              <div className='d-flex margin-left'>
                 <span>todos</span>
                 <span>ativo</span>
                 <span>completado</span>
